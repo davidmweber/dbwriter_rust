568af40fd2597a0ac226abc13849cdaffd0a4cea
David Weber <dave@veryflatcat.com>
2021-10-04 08:53:01 +0200

    Figuring packaging and imports out.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate serde_json;
+//
 pub mod schema;
-use diesel::prelude::*;
+use schema::sample;
+// use schema::sample::dsl::sample;
 use diesel::pg::PgConnection;
+// use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
-use crate::schema::sample::columns::{name, timestamp};
-use crate::schema::sample::dsl::sample;
+use diesel::{Connection, RunQueryDsl};
+
+#[derive(Insertable)]
+#[table_name = "sample"]
+pub struct AddSample<'a> {
+    name: &'a str,
+    timestamp: Option<&'a str>,
+    // I really don't know what &'a means in this context
+    v0: &'a Option<&'a f64>,
+    v1: &'a Option<&'a f64>,
+}
 
 fn main() {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let connection = PgConnection::establish(&db_url)
         .expect(&format!("Error connecting to database at {}", db_url));
-
-    let record = (&name.eq("foo"), &timestamp.eq()
-    let inserts = diesel::insert_into(sample)
-        .values(&name.eq("foo")).execute(&connection);
-    print!("{} inserts", inserts.unwrap())
+    for i in 0..10 {
+        let record = AddSample {
+            name: "floof",
+            timestamp: None,
+            v0: &Some(&(i as f64)),
+            v1: &Some(&((2 * i) as f64))
+        };
+        let inserts = diesel::insert_into(sample::dsl::sample)
+            .values(record)
+            .execute(&connection);
+    }
 }
