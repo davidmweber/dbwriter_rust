b5af0015c7f4a499245419fb67cc4bbedf2aabdf
David Weber <dave@veryflatcat.com>
2021-10-11 14:52:03 +0200

    Working getter on DB with error handler

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ extern crate chrono;
 extern crate diesel;
 mod schema;
 
-use std::env;
 use dotenv::dotenv;
+use std::env;
 
-use actix_web::{get, post, App, HttpServer, web, HttpResponse, Error};
-use actix_web::error::BlockingError;
+use actix_web::rt::blocking::BlockingError;
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer};
 use chrono::prelude::*;
-use diesel::prelude::*;
-use diesel::{QueryDsl, r2d2};
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use diesel::result::Error::NotFound;
-use serde::{Deserialize, Serialize};
+use diesel::{r2d2, QueryDsl};
 use schema::samples;
+use serde::{Deserialize, Serialize};
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
@@ -44,27 +44,42 @@ async fn get_samples(pool: web::Data<DbPool>) -> &'static str {
 }
 
 #[get("/samples/{sample_id}")]
-async fn get_sample(pool: web::Data<DbPool>, sample_id: web::Path<i64>)  -> Result<HttpResponse, Error> {
+async fn get_sample(
+    pool: web::Data<DbPool>,
+    sample_id: web::Path<i64>,
+) -> Result<HttpResponse, Error> {
     use schema::samples::dsl::*;
     let s_id = sample_id.into_inner();
     let conn = match pool.get() {
         Ok(c) => c,
-        Err(e) => return Ok(HttpResponse::InternalServerError().body(format!("Failed to get a database connection: {}", e)))
+        Err(e) => {
+            return Ok(HttpResponse::InternalServerError()
+                .body(format!("Failed to get a database connection: {}", e)))
+        }
     };
 
     // Shunt this to a thread pool so it does not block here.
-    let s =  web::block(move || {
-        samples.filter(id.eq(&s_id)).first::<Sample>(&conn)
-    }).await;
+    let s = web::block(move || samples.filter(id.eq(&s_id)).first::<Sample>(&conn))
+        .await;
+//        .map_err(|e| HttpResponse::InternalServerError().finish())?;
 
     // The Result we get from the block operation wraps the original error in a blocking
     // error so we check first if the operation was cancelled. If not, we dig the original
     // error out and deal with it
     match s {
         Ok(r) => Ok(HttpResponse::Ok().json(r)),
-        BlockingError::Canceled() => Ok(HttpResponse::InternalServerError().body("Database request cancelled on blocking")),
-        BlockingError::Error(NotFound) => Ok(HttpResponse::NotFound().body(format ! ("Sample not found {}", s_id))),
-        BlockingError::Error(e) => Ok(HttpResponse::InternalServerError().body(format ! ("Unknown database error {}", e)))
+        Err(e) => {
+            match e {
+                // I suspect there is a more elegant way
+                BlockingError::Canceled => Ok(HttpResponse::InternalServerError()
+                    .body("Database request cancelled on blocking")),
+                BlockingError::Error(NotFound) => {
+                    Ok(HttpResponse::NotFound().body(format!("Sample not found {}", s_id)))
+                }
+                BlockingError::Error(e) => Ok(HttpResponse::InternalServerError()
+                    .body(format!("Unknown database error {}", e))),
+            }
+        }
     }
 }
 
@@ -93,23 +108,24 @@ async fn main() -> std::io::Result<()> {
             name: String::from("frobnicator_manifold_pressure"),
             timestamp: Utc::now().naive_utc(),
             v0: Some(i as f32),
-            v1:Some((2 * i) as f32),
+            v1: Some((2 * i) as f32),
         };
         let res = diesel::insert_into(samples::dsl::samples)
             .values(record)
             .execute(&conn);
-            assert_eq!(res, Ok(1)); // Will be Err(something) if there was a problem
+        assert_eq!(res, Ok(1)); // Will be Err(something) if there was a problem
     }
     //let results = sample.ilter
 
     // Start up the HTTP server, set up the routes and and just block on its completion
-    HttpServer::new(move || {  // This is a lambda function with zero arguments
+    HttpServer::new(move || {
+        // This is a lambda function with zero arguments
         App::new()
             .data(pool.clone())
             .service(hello)
             .service(get_samples)
             .service(get_sample)
-            //.service(write_samples)
+        //.service(write_samples)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
